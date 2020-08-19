cb99bc07771e65c11b829645f1edd7d5792a1ca8
phil <tchatchoua1806@gmail.com>
2020-08-19 17:01:32 +0200

    Installation de l'admin et migration de la db

---
+++ database/migrations/2020_08_19_145307_create_table_news.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableNews extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('image');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
